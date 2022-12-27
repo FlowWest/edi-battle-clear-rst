991327f9bef0bd95ba33354d22b6cfb750a84ba5
liz-fw <lstebbins@flowwest.com>
2022-12-27 08:04:33 -0800

    creates cleaned_trap table

--- data-raw/pull_raw_data_from_database.R
+++ data-raw/pull_raw_data_from_database.R
@@ -27,18 +27,20 @@ trap_sample <- mdb.get(filepath, "Sample") |> glimpse()
 run_lu <- mdb.get(filepath, "RaceList") |> glimpse()
 lifestage_lu <- mdb.get(filepath, "StagesLookUp") |> glimpse()
 stations_lu <- mdb.get(filepath, "StationsLookUp")  |> glimpse()
-organisim_lu <- mdb.get(filepath, "OrganismsLookUp") |> glimpse()
+organism_lu <- mdb.get(filepath, "OrganismsLookUp") |> glimpse()
 
 
 View(catch_raw)
 View(trap_sample)
+
+# cleaned_catch
 
 cleaned_catch <- catch_raw |> 
   full_join(trap_sample, by = c("SampleRowID" = "SampleRowID")) |> 
   left_join(run_lu, by = c("Race" = "RaceCode")) |> 
   left_join(lifestage_lu, by = c("LifeStage" = "LifeStage")) |> 
   left_join(stations_lu, by = c("StationCode" = "StationCode")) |> 
-  left_join(organisim_lu, by = c("OrganismCode" = "OrganismCode")) |> 
+  left_join(organism_lu, by = c("OrganismCode" = "OrganismCode")) |> 
   select(SampleID, SampleDate, SampleTime, Location, StationCode, 
          CommonName, Count, ForkLength, Weight, Race, LifeStage = StageName, 
          RCatch, Interp, BroodYear) |> 
@@ -62,3 +64,13 @@ cleaned_catch <- catch_raw |>
 unique(cleaned_catch$Run) # why no fall run 
 unique(cleaned_catch$Location)
 unique(cleaned_catch$LifeStage)
+
+# cleaned_trap
+cleaned_trap <- trap_sample |> 
+  left_join(stations_lu, by = c("StationCode" = "StationCode")) |> 
+  select(SampleID, SampleDate, SampleTime, StationCode, Location, 
+         TrapFishing, Counter, FlowStartMeter, FlowEndMeter, StartCounter,
+         Velocity, Turbidity) |> 
+  glimpse()
+# TODO temp and discharge?
+  
