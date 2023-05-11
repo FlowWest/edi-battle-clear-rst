66167d2c5ca38d8c56d7555f1bc02abe723be124
liz-fw <lstebbins@flowwest.com>
2023-05-11 12:40:35 -0700

    reads in updated database and formats catch and trap tables

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -5,31 +5,10 @@ library(lubridate)
 
 # TODO decide which catch to use
 # catch
-catch_2022 <- read_csv(here::here("data", "catch_2022.csv")) |> 
-  mutate(dead = if_else(dead == "Y", "yes", "no", dead)) |> 
-  glimpse()
-
-catch <- read_csv(here::here("data", "catch.csv")) |> 
-  glimpse()
-
-# compare two catch tables
-catch_2022 |> summarise(range(date)) # 2022-10-01 : 2023-01-05
-catch |> summarise(range(sample_date)) # 2022-10-01 : 2022-11-21
-
-# catch 2022 does not have an explicit catch date - it's the FLTS variable
+catch <- read.csv(here::here("data", "catch_final.csv")) |> glimpse()
 
 # trap
-# TODO confirm this is the table we should use
-trap_2022 <- read_csv(here::here("data", "trap_2022.csv")) |> 
-  mutate(sample_time = as.character(sample_time)) |> 
-  glimpse()
-
-trap <- read_csv(here::here("data", "trap.csv")) |>
-  glimpse()
-
-# compare two catch tables
-trap_2022 |> summarise(range(sample_date)) # 2022-10-01 : 2023-01-05
-trap |> summarise(range(sample_date)) # 2022-10-01 : 2022-11-21
+trap <- read.csv(here::here("data", "trap_final.csv")) |> glimpse()
 
 
 # recapture - clear
@@ -49,3 +28,4 @@ release_clear <- read_csv(here::here("data", "clear_release.csv")) |>
 release_battle <- read_csv(here::here("data", "battle_release.csv")) |> 
   mutate(time_released = as.character(time_released)) |> 
   glimpse()
+
--- data-raw/pull_raw_data_from_database.R
+++ data-raw/pull_raw_data_from_database.R
@@ -163,3 +163,107 @@ write_csv(trap, here::here("data", "trap_2022.csv"))
 
 
 
+# 2020-2022 database ------------------------------------------------------
+# read in mdb
+library(Hmisc)
+filepath <- here::here("data-raw", "scripts_and_data_from_natasha",
+                       "RST_Database_2020-07-06_to_2022-09-30.accdb")
+tables <- mdb.get(filepath, tables = TRUE)
+tables
+# data tables are: BioSample, Catch, Sample, HistoricalSampleID
+# data
+catch_raw <- mdb.get(filepath, "Catch") |> glimpse()
+trap_raw <- mdb.get(filepath, "Sample") |> glimpse()
+
+# store all LU tables in a named list
+LU_table_names <- tables[! tables %in% c("Catch", "Sample")]
+LU_tables <- sapply(LU_table_names, function(x){
+  return(mdb.get(filepath, paste0(x)))
+}, USE.NAMES = T)
+detach(package:Hmisc)
+
+
+# catch -------------------------------------------------------------------
+
+# TODO dates go up to 2029
+catch <- catch_raw |> 
+  left_join(LU_tables$OrganismsLookUp |> 
+              select(OrganismCode, CommonName),
+            by = "OrganismCode") |> 
+  left_join(LU_tables$RaceList |> 
+              select(RaceCode, Race_Description = Description),
+            by = c("Race" = "RaceCode")) |> 
+  left_join(LU_tables$RaceList |> 
+              select(RaceCode, FWS_Race_Description = Description),
+            by = c("FWSRace" = "RaceCode")) |> 
+  left_join(LU_tables$RaceList |> 
+              select(RaceCode, Report_Race_Description = Description),
+            by = c("ReportRace" = "RaceCode")) |> 
+  left_join(LU_tables$StagesLookUp |> 
+              select(LifeStage, StageName),
+            by = "LifeStage") |> 
+  select(-c(OrganismCode, Race, LifeStage, FWSRace, ReportRace)) |> 
+  rename(LifeStage = StageName) |> 
+  mutate(LifeStage = str_remove_all(LifeStage, "RBT - "),
+         LifeStage = str_remove_all(LifeStage, "CHN - "),
+         Race = if_else(Race_Description == "N/P", "not provided", Race_Description),
+         FWS_Race = if_else(FWS_Race_Description == "N/P", "not provided", FWS_Race_Description),
+         Report_Race = if_else(Report_Race_Description == "N/P", "not provided", Report_Race_Description),
+         Subsample = if_else(Subsample == "N/P", "not provided", Subsample),
+         FishFLTS = gsub("[()]", "", FishFLTS),
+         Date = mdy(str_replace_all(str_sub(FishFLTS, 1, 8), "/", "-"))) |>
+  select(-c(FWS_Race_Description, Race_Description, Report_Race_Description)) |> 
+  clean_names() |> 
+  glimpse()
+
+catch |> filter(year(date) > 2023) |> glimpse()
+
+write_csv(catch, here::here("data", "catch_final.csv"))
+
+# trap
+trap <- trap_raw |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "WeatherList") |> 
+              select(ValueCode, Weather = CodeDescription),
+            by = c("WeatherCode" = "ValueCode")) |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "HabitatList") |> 
+              select(ValueCode, Habitat_Description = CodeDescription),
+            by = c("Habitat" = "ValueCode")) |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "TrapSampleTypeList") |> 
+              select(ValueCode, TrapSampleType_Description = CodeDescription),
+            by = c("TrapSampleType" = "ValueCode")) |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "DielList") |> 
+              select(ValueCode, Diel_Description = CodeDescription),
+            by = c("Diel" = "ValueCode")) |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "DebrisTypeList") |> 
+              select(ValueCode, DebrisType_Description = FormDisplay),
+            by = c("DebrisType" = "ValueCode")) |> 
+  left_join(LU_tables$LunarPhaseLookUp |> 
+              select(LunarPhase = strPhase, 
+                     lunar_phase_description = formDisplay),
+            by = c("LunarPhase")) |> 
+  left_join(LU_tables$VariableCodesLookUp |> 
+              filter(CodeListName == "ConditionList") |> 
+              select(ValueCode, Gear_Description = CodeDescription),
+            by = c("GearConditionCode" = "ValueCode")) |> 
+  mutate(SampleTime = str_sub(as.character(SampleTime), 11, 18),
+         SampleTime = if_else(SampleTime == "", NA_character_, SampleTime),
+         TrapStartTime = str_sub(as.character(TrapStartTime), 11, 18),
+         TrapStartTime = if_else(TrapStartTime == "", NA_character_, TrapStartTime),
+         Thalweg = ifelse(Thalweg == "Y", "Yes", "No")) |> 
+  select(-c(WeatherCode, Habitat, TrapSampleType, Diel, DebrisType,
+            UserName, UserName2, LunarPhase, GearConditionCode)) |> 
+  rename(Habitat = Habitat_Description, 
+         TrapSampleType = TrapSampleType_Description,
+         Diel = Diel_Description,
+         DebrisType = DebrisType_Description,
+         LunarPhase = lunar_phase_description,
+         GearCondition = Gear_Description) |> 
+  clean_names() |> 
+  glimpse()
+
+write_csv(trap, here::here("data", "trap_final.csv"))
