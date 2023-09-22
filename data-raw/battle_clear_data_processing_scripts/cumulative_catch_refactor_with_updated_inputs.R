7ad9177d3408fb98d7acd05fcc79d64a4edd0f66
liz-fw <lstebbins@flowwest.com>
2023-09-22 11:55:21 -0700

    updates cumulative catch refactor script

--- data-raw/battle_clear_data_processing_scripts/cumulative_catch_refactor_with_updated_inputs.R
+++ data-raw/battle_clear_data_processing_scripts/cumulative_catch_refactor_with_updated_inputs.R
@@ -1,27 +1,12 @@
 # This code will calculate daily catch, cumulative catch, and percent cumulative catch
 # for brood years at the lower Clear Creek rotary screw trap. The data comes from 4 queries
 # Report Data CHN, Report Data WCS, Report Data LFFCS, and Report Data RBT
-# Mike Schraml 02/02/2022
 
-
-# This script is a refactor of Cumulative catch LCC.R
-# Mike Schraml, USFWS
-# 05-23-2023
-
-# Load needed Packages
-if (!require("readxl")) {            # for importing data sets
-  install.packages("readxl")
-  library(readxl)
-}
+# This script is a refactor of code by Mike Schraml, USFWS
 
 if (!require("tidyverse")) {            # data management
   install.packages("tidyverse")
   library(tidyverse)
-}
-
-if (!require("openxlsx")) {         # for writing xlsx files to the desk top
-  install.packages("openxlsx")
-  library(openxlsx)
 }
 
 if (!require("padr")) {         # for for inserting missing dates
@@ -29,28 +14,20 @@ if (!require("padr")) {         # for for inserting missing dates
   library(padr)
 }
 
-if (!require("lubridate")) {         # for for dealing with dates
-  install.packages("lubridate")
-  library(lubridate)
-}
-
-
 # prep data ---------------------------------------------------------------
 
-catch <- read.csv(here::here("data", "catch.csv")) |> 
+catch <- read.csv("data/catch.csv") |> 
   select(date = sample_date, brood_year, fws_run, r_catch, 
          common_name, station_code, interp) |> 
   glimpse()
 
-
 # cumulative catch --------------------------------------------------------
-# by <- 2019 
-by <- 2021 # 2019 is original value; too restrictive for checking code
 
-sample_data <- catch |> # TODO how to get station code?
-  filter(brood_year == by, 
-         interp == FALSE,
+# brood_year_filter <- 2021 # use this if you want to filter
+sample_data <- catch |> 
+  filter(interp == FALSE,
          common_name %in% c("Chinook Salmon", "Rainbow Trout")) |> 
+  #filter(brood_year %in% brood_year_filter) |> 
   glimpse()
 
 min_date <- as.Date(min(sample_data$date, na.rm = T))
@@ -62,12 +39,13 @@ sample_data_full <- sample_data |>
   pad(interval = "day",
       start_val = min_date,
       end_val = max_date) |>
-  fill_by_value(r_catch, value = 0) |> glimpse()
+  fill_by_value(r_catch, value = 0) |> 
+  glimpse()
 
 daily_catch <- sample_data_full |>
   group_by(date, fws_run, common_name, station_code) |> 
   summarise(catch = sum(r_catch, na.rm = TRUE),
-            cumulative_catch = cumsum(catch)) |> # TODO move total_catch into summarise?
+            cumulative_catch = cumsum(catch)) |> 
   mutate(total_catch = sum(catch, na.rm = TRUE), 
          cumulative_percent_catch = round((cumulative_catch / total_catch * 100), 0),
          cumulative_percent_catch = ifelse(cumulative_percent_catch == "NaN", NA, cumulative_percent_catch)) |> 
@@ -80,9 +58,8 @@ daily_catch_wide <- daily_catch |>
               names_from = c(station_code, fws_run, common_name),
               values_from = cumulative_percent_catch) |> 
   glimpse()
-  
 
-# write file
-write.csv(daily_catch_wide, here::here("data-raw", "cumulative_catch.csv"),
-          row.names = FALSE)
+
+# write file --------------------------------------------------------------
+# as appropriate
 
